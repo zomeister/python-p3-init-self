fea8b0bc0158c288ddaefbdc9baba2841d8eeb01
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 21:13:01 -0500

    lab completed.

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name):
+        self.name = name
     pass
